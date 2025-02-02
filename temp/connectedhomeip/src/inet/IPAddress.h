5171b448ed73218c1d7967a7f231a8c11cd22c86
halac123b <halac123b@gmail.com>
2025-02-02 13:12:22 +0700

    Add temp m_connectedhomeip

---
+++ temp/connectedhomeip/src/inet/IPAddress.h
@@ -0,0 +1,9 @@
+/**
+ *    @file
+ *      This file defines the class <tt>Inet::IPAddress</tt> and
+ *      related enumerated constants. The CHIP Inet Layer uses objects
+ *      of this class to represent Internet protocol addresses of both
+ *      IPv4 and IPv6 address families. (IPv4 addresses are stored
+ *      internally as IPv4-Mapped IPv6 addresses.)
+ */
+#pragma once
