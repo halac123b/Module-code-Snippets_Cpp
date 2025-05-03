a4acb5ca9fa5a7e07e022cd5cf11ca6ab6b8417a
halac123b <halac123b@gmail.com>
2025-05-03 12:25:58 +0700

    Add m_boostAsio

---
+++ temp/boost_asio/include/boost/asio.hpp
@@ -0,0 +1,10 @@
+#ifndef BOOST_ASIO_HPP
+#define BOOST_ASIO_HPP
+
+#if defined(_MSC_VER) && (_MSC_VER >= 1200)
+#pragma once
+#endif // defined(_MSC_VER) && (_MSC_VER >= 1200)
+
+#include <boost/asio/any_completion_executor.hpp>
+
+#endif // BOOST_ASIO_HPP
---
+++ temp/boost_asio/include/boost/asio/any_completion_executor.hpp
@@ -0,0 +1,9 @@
+#ifndef BOOST_ASIO_ANY_COMPLETION_EXECUTOR_HPP
+#define BOOST_ASIO_ANY_COMPLETION_EXECUTOR_HPP
+
+#if defined(_MSC_VER) && (_MSC_VER >= 1200)
+#pragma once
+#endif // defined(_MSC_VER) && (_MSC_VER >= 1200)
+
+#include <boost/asio/detail/config.hpp>
+#endif
